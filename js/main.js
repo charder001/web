aff106532419db847fd98864be00d7e1c656fd1c
charder001 <43436118+charder001@users.noreply.github.com>
2019-01-30 15:51:16 +0100

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,10 +1,19 @@
 var hart = document.querySelectorAll(".fav");
 var donehart = document.querySelectorAll(".donefav");
+var popup = document.querySelector(".popup");
+var badge = document.querySelector(".badge");
+var download = document.getElementById("download");
 
 console.log(hart);
 
 hart[0].addEventListener('click', function(){
 	hart[0].classList.toggle("filledfav");
+	badge.classList.toggle("badgetoggle");
+
+});
+
+hart[1].addEventListener('click', function(){
+	popup.classList.toggle("popuptoggle");
 });
 
 
@@ -13,3 +22,37 @@ donehart[0].addEventListener('click', function(){
 	hart[0].classList.toggle("filledfav");
 });
 
+/*
+download.addEventListener("click", function(){
+  var elem = document.getElementById("Bar"); 
+  var width = 1;
+  var id = setInterval(frame, 10);
+  function frame() {
+    if (width >= 100) {
+      clearInterval(id);
+      document.getElementById("Bar").style.backgroundColor = "green";
+    } else {
+      width++; 
+      elem.style.width = width + '%'; 
+      elem.innerHTML = width * 1 + '%';
+    }
+  } elem.classList.toggle("bartoggle");
+});*/
+
+
+download.addEventListener("click", function(){
+  var elem = document.getElementById("Bar"); 
+  var width = 1;
+  var id = setInterval(frame, 10);
+  function frame() {
+    if (width >= 65) {
+      clearInterval(id);
+      document.getElementById("Bar").style.backgroundColor = "red";
+      elem.innerHTML = 'Geen internet verbinding';
+    } else {
+      width++; 
+      elem.style.width = width + '%'; 
+      elem.innerHTML = width * 1 + '%';
+    }
+  } elem.classList.toggle("bartoggle");
+});
